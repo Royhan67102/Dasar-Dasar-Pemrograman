980e9cfaf98bec14d9fb2ec67cefb1dc91340e01
Royhan67102 <99383807+Royhan67102@users.noreply.github.com>
2023-11-01 20:08:51 +0700

    Add files via upload

---
+++ praktikum6/hello.py
@@ -0,0 +1,64 @@
+a = "STT Nurul Fikri"
+b = 0
+while b<=100:
+    print (a)
+    b+=1
+
+
+a=99
+while a>=1:
+    print (a)
+    a-=2
+
+
+#for i in range (1,8):
+ #   print(i)
+
+
+for i in range (50):
+    print(i)
+
+
+for i in range (0,31,3):
+    print (i)
+#0 itu awal mulai,itu hasil akhir,3 itu stepnya
+
+
+for i in range (5,55,5):
+    print (i)
+
+
+for i in range (50,0,-5):
+    print (i)
+
+for i in range (3):
+    print ("TI02")
+    #i variabel bebas
+
+a=1 # = 1 untuk memberi nilai
+while a<6:
+    print(a)
+    if a ==3: # == 2 untuk membandingkan nilai
+        break
+    a+=1 # a=1
+
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3: #nilai yang di skip
+    continue #diskip 1 kali
+  print(i)
+
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i tidak lebih besar dari  6")
+
+
+
+
+    
